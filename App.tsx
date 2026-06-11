27712a6ed69a3dc62aed1a7c51a39bca21ba349e
YZZlD <max.verhulst@hotmail.com>
2026-06-11 14:52:43 -0600

    chore: initial commit with folder structure

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+
+export default function App() {
+  return (
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
+});
