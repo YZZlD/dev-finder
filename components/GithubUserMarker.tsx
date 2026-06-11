7d15b3bbc0a023fb00b58fd6a7f08e4db92ab668
YZZlD <max.verhulst@hotmail.com>
2026-06-11 16:33:49 -0600

    feat: map screen created

---
+++ components/GithubUserMarker.tsx
@@ -0,0 +1,17 @@
+type GithubUser = {
+        id: number;
+        name: string;
+        latitude: number;
+        longitude: number;
+    }
+
+type Props = {
+    user: GithubUser
+};
+
+export default function GithubUserMarker({ user } : Props)
+{
+    return (
+        <
+    );
+}
--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -1,6 +1,24 @@
+import MapView from "react-native-maps";
+import GithubUserMarker from "../components/GithubUserMarker";
+import users from "../data/users.json";
+
 export default function MapScreen()
 {
     return (
-        <></>
+        <MapView style={{ flex: 1}} initialRegion={{
+            latitude: 37.7879,
+            longitude: 122.4074,
+            latitudeDelta: 0.10,
+            longitudeDelta: 0.10
+        }}>
+            {users.map(user => {
+                return (
+                    <GithubUserMarker
+                        key={user.id}
+                        user={user}
+                    />
+                )
+            })};
+        </MapView>
     );
 }
